1e7f607962b17dde3971fe28d8ff2dac531e8022
MichaelASC23 <michaeljhleung@gmail.com>
2023-08-14 12:06:44 -0400

    start

---
+++ script.js
@@ -0,0 +1,16 @@
+// References
+let logo = document.getElementById("logo");
+let about = document.getElementById("about");
+let welcome = document.getAnimations("welcome");
+
+// Home Button
+logo.onclick = function(event){
+    event.preventDefault();
+    location.href = main.html;
+}
+
+//About button 
+about.onclick = function(event){
+    event.preventDefault();
+    
+}
